9a9b15f688586b2ea5ab2454b0b64b72e9a5cb7d
杨健峰 <1677306192@qq.com>
2025-05-23 18:22:43 +0800

    springCloud集成zipkin

---
+++ zipkinClient/src/test/java/com/example/zipkinClient/ZipkinClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zipkinClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
